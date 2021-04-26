0e18edf0f17d4cc307d505b27eb10874e7be863e
Julia Sharon <62422650+Jul-Sharon@users.noreply.github.com>
2021-04-26 20:39:31 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,1 @@
 'use strict';
-let isMobile = {
-	Android: function () {return navigator.userAgent.match(/Android/i);},
-	BlackBerry: function () {return navigator.userAgent.match(/BlackBerry/i);},
-	IOS: function () {return navigator.userAgent.match(/IPhone|IPad|IPod/i);},
-	Opera: function () {return navigator.userAgent.match(/Opera Mini/i);},
-	Windows: function () {return navigator.userAgent.match(/IEMobile/i);},
-	any: function () {return (isMobile.Android()|| isMobile.BlackBerry()|| isMobile.IOS()|| isMobile.Opera()|| isMobile.Windows());}};
-if (isMobile.any()){
-function changeStyle(){
-              document.getElementByClassName('.mainlink').style.color = 'red';
-           }}
